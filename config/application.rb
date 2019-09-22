b44c0ba2e89188eb1f18202a3e9a02998c4fc14a
eshnil <team@polyglot.network>
2019-09-22 12:55:48 +0530

    copy changes, social icons for authors

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Booker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
 
